fef0a92463a0550c79e4c4d5573923211d506aeb
xJesusx0 <jesuspl0411@gmail.com>
2024-09-26 15:35:21 -0500

    db actualizada

--- res.sql
+++ res.sql
@@ -1,22 +1,22 @@
 CREATE TABLE `users` (
-  `user_id` int PRIMARY KEY,
-  `username` varchar(255),
-  `password` varchar(255),
-  `role` varchar(255)
+  `user_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
 );
 
 CREATE TABLE `products` (
-  `product_id` int PRIMARY KEY,
-  `name` varchar(255),
-  `price` decimal(10,2),
-  `amount` int
+  `product_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL UNIQUE,
+  `price` decimal(10,2) NOT NULL,
+  `amount` int NOT NULL
 );
 
 CREATE TABLE `purchases` (
-  `user_id` int,
-  `product_id` int,
-  `quantity` int,
-  `purchase_date` datetime,
+  `user_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `purchase_date` datetime NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(user_id),
   FOREIGN KEY(product_id) REFERENCES products(product_id),
   PRIMARY KEY (`user_id`, `product_id`, `purchase_date`)
